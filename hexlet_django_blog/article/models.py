28544eed86cc673eb240c8621503c0387245c74d
fooxeed <fooxeed@mail.ru>
2024-07-14 18:06:26 +1000

    Create article

--- hexlet_django_blog/article/models.py
+++ hexlet_django_blog/article/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class Article(models.Model):
     name = models.CharField(max_length=200)
     body = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'Name: {self.name}'
